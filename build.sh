9c1ec258d3b660d48b7d6f76e3243894decd34ea
steden <steden@fsgit.com>
2026-03-17 15:58:47 +0800

    format

---
+++ build.sh
@@ -0,0 +1,13 @@
+#mac
+GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -o ./dist/fops-agent.Darwin.x86_64 -ldflags="-w -s" .
+GOOS=darwin GOARCH=arm64 CGO_ENABLED=0 go build -o ./dist/fops-agent.Darwin.arm64 -ldflags="-w -s" .
+    
+#linux64
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o ./dist/fops-agent.Linux.x86_64 -ldflags="-w -s" .
+GOOS=linux GOARCH=386 CGO_ENABLED=0 go build -o ./dist/fops-agent.Linux.i686 -ldflags="-w -s" .
+#windows
+GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -o ./dist/fops-agent.Windows.x86_64 -ldflags="-w -s" .
+GOOS=windows GOARCH=386 CGO_ENABLED=0 go build -o ./dist/fops-agent.Windows.i686 -ldflags="-w -s" .
+
+# 本机执行
+#scp root@192.168.1.8:/home/code/FOPS-Agent/dist/* ~/code/FOPS-Agent
